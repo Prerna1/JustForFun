ea925cecff6a566fef4476e55e0e2cf0b96080c0
Prerna1 <prernasuneja52.ps@gmail.com>
2018-10-06 15:23:45 +0530

    Initial Commit

--- app/src/main/java/com/example/sunejas/justforfun/MainActivity.java
+++ app/src/main/java/com/example/sunejas/justforfun/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.sunejas.justforfun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button playButton,scoreButton,tvShowButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        playButton = findViewById(R.id.play_button);
+        scoreButton = findViewById(R.id.score_button);
     }
 }
